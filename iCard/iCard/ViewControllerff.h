87c9a08a895a7cddd64277eb20c7a9cb0117808d
Daniele <Daniele@psych-ii.pegaso.dom>
2012-10-30 14:40:36 +0100

    faccio er push

---
+++ iCard/iCard/ViewControllerff.h
@@ -0,0 +1,14 @@
+//
+//  ViewControllerff.h
+//  iCard
+//
+//  Created by Daniele on 27/10/12.
+//  Copyright (c) 2012 Cards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface ViewControllerff : UIViewController
+-(IBAction)onEgggit:(id)sender;
+@end
